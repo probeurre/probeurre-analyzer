763723e3d3a8dfd3779a873a3fc5caff97b83d30
christop.blanquet <christophe.blanquet@edu.hefr.ch>
2017-03-30 18:38:01 +0200

    comment the todo search regex

--- Todo_Parser.py
+++ Todo_Parser.py
@@ -7,6 +7,7 @@ with open('sample.json', encoding='utf-8') as data_file:
     json_data = json.loads(data_file.read())
 
 def extractContent(dict):
+    # "line start" OR "any separator" ... _todo_ .. "line end" or "any separator"
     reg = re.compile("(^|[\s,.])todo([\s,.]|$)", re.IGNORECASE)
     nbTodo = 0
     for fileName in dict:
