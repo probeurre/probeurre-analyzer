cf3c7f08e16eb75f7f630d35479ef00a8ab96139
christop.blanquet <christophe.blanquet@edu.hefr.ch>
2017-03-30 18:24:15 +0200

    todo counter, using regex to find todo 'words' only

---
+++ Todo_Parser.py
@@ -0,0 +1,20 @@
+import json
+import re
+from pprint import pprint
+
+# TODO : get arg instead of sample file
+with open('sample.json', encoding='utf-8') as data_file:
+    json_data = json.loads(data_file.read())
+
+def extractContent(dict):
+    reg = re.compile("(^|[\s,.])todo([\s,.]|$)", re.IGNORECASE)
+    nbTodo = 0
+    for fileName in dict:
+        for line in dict[fileName]:
+            if reg.search(dict[fileName][line]["content"]):
+                print("Found a todo >>", dict[fileName][line]["content"])
+                nbTodo+=1
+    return nbTodo
+
+result = extractContent(json_data)
+print("total number of todo : ", result)
